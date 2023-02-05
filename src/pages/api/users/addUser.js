b6990d18ef766732db90fb1f43e1cc89b8e39c7e
Matthew Powell <matt.powell2784@gmail.com>
2023-02-05 13:49:33 +0000

    prisma initial commit

---
+++ src/pages/api/users/addUser.js
@@ -0,0 +1,36 @@
+import { prisma } from '../../../../server/db/client'
+
+export default async function handler(req, res) {
+  const { method } = req
+
+  try {
+    if (method === 'POST') {
+      const { email, firstName, lastName, password } = req.body
+
+      const userExists = await prisma.User.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userExists) {
+        return res
+          .status(400)
+          .json({ success: false, status: 400, errors: [{ msg: 'User already exists.' }] })
+      }
+
+      const user = await prisma.User.create({
+        data: { email, firstName, lastName, password },
+      })
+
+      res.status(201).json({
+        success: true,
+        status: 201,
+        user,
+      })
+    }
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send({ success: false, status: 500, errors: [{ msg: 'Server Error' }] })
+  }
+}
