a698f955989641ced38e89c2add9fc10fceca38a
Matthew Powell <matt.powell2784@gmail.com>
2023-02-08 16:27:09 +0000

    hero image updated;=

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -2,15 +2,34 @@ import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'nex
 import { getProviders, signIn } from 'next-auth/react'
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../api/auth/[...nextauth]'
+import React from 'react'
+import styled from 'styled-components'
+import Image from 'next/image'
 
 export default function SignIn({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>Sign in with {provider.name}</button>
-        </div>
-      ))}
+      <HeroSectionStyled>
+        <LeftSectionFlexboxStyled>
+          <SignInContainer>
+            {Object.values(providers).map((provider) => (
+              <div key={provider.name}>
+                <button onClick={() => signIn(provider.id)}>Sign in with {provider.name}</button>
+              </div>
+            ))}
+          </SignInContainer>
+        </LeftSectionFlexboxStyled>
+        <RightSectionFlexboxStyled>
+          <ImageContainerStyled>
+            <Image
+              src="/homepage_hero_lake_v3.jpg"
+              fill
+              style={{ objectFit: 'cover' }}
+              alt="Lady in canoe on a lake"
+            />
+          </ImageContainerStyled>
+        </RightSectionFlexboxStyled>
+      </HeroSectionStyled>
     </>
   )
 }
@@ -31,3 +50,46 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     props: { providers: Object.values(providers) ?? [] },
   }
 }
+
+const HeroSectionStyled = styled.section`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  background: var(--bg-primary);
+`
+
+const LeftSectionFlexboxStyled = styled.div`
+  position: absolute;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  @media only screen and (max-width: 992px) {
+    width: 100%;
+  }
+`
+
+const SignInContainer = styled.div`
+  position: absolute;
+`
+
+const RightSectionFlexboxStyled = styled.div`
+  position: absolute;
+  right: 0;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 992px) {
+    width: 100%;
+  }
+`
+const ImageContainerStyled = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
