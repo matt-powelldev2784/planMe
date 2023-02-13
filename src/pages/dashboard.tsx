804e095c60c331225f6ae51b38f6b69c4a885822
Matthew Powell <matt.powell2784@gmail.com>
2023-02-13 19:46:58 +0000

    haboard typescript updated

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,21 +3,25 @@ import { useEffect } from 'react'
 import { Dashboard } from '@/components'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import { Session } from 'next-auth/core/types'
 
 export default function DashboardPage() {
-  const { data: session, loading } = useSession()
+  const { data: session, status } = useSession()
   let router = useRouter()
   const [routerCalled, setRouterCalled] = useState(false)
 
   useEffect(() => {
+    console.log('session', session)
+    console.log('status', status)
+
     const redirectIfNoSession = () => {
-      if (!routerCalled && !loading && !session) {
+      if (status === 'unauthenticated') {
         router?.push('/')
         setRouterCalled(true)
       }
     }
     redirectIfNoSession()
-  }, [session, loading, router, routerCalled])
+  }, [session, router, routerCalled, status])
 
   if (session) {
     return (
