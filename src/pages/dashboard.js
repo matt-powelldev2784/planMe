80bd2d248a29d16c4b17fa21f9e520eb813e8b37
Matthew Powell <matt.powell2784@gmail.com>
2023-02-13 18:37:25 +0000

    redirect to home if not authorized

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,23 @@
 import { useSession, signIn, signOut } from 'next-auth/react'
+import { useEffect } from 'react'
 import { Dashboard } from '@/components'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 export default function DashboardPage() {
-  const { data: session } = useSession()
+  const { data: session, loading } = useSession()
+  let router = useRouter()
+  const [routerCalled, setRouterCalled] = useState(false)
+
+  useEffect(() => {
+    const redirectIfNoSession = () => {
+      if (!routerCalled && !loading && !session) {
+        router?.push('/')
+        setRouterCalled(true)
+      }
+    }
+    redirectIfNoSession()
+  }, [session, loading, router, routerCalled])
 
   if (session) {
     return (
