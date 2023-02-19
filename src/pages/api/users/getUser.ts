d15422212a1146b6c5a64c72aef6e6edfedd166f
Matthew Powell <matt.powell2784@gmail.com>
2023-02-19 08:36:34 +0000

    tsconfig updated

--- src/pages/api/users/getUser.ts
+++ src/pages/api/users/getUser.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from './../auth/[...nextauth]'
+import { authOptions } from '../auth/[...nextauth]'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { User, apiError } from '@/ts/interfaces'
 
