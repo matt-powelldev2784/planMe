a426a1c9e3a8e187789477693dcaab610829f069
Matthew Powell <matt.powell2784@gmail.com>
2023-02-19 11:04:00 +0000

    unused prisma cleint removed

--- server/db/client.js
+++
@@ -1,9 +0,0 @@
-// import { PrismaClient } from '@prisma/client'
-
-// export const prisma =
-//   global.prisma ||
-//   new PrismaClient({
-//     //log: ['query'], //logs prisma query to terminal
-//   })
-
-// if (process.env.NODE_ENV !== 'production') global.prisma = prisma
