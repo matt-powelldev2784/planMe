5c6db5f11d92370e693ea82ccce8575e87878c29
Matthew Powell <matt.powell2784@gmail.com>
2023-02-22 20:15:30 +0000

    rtk useWrappedStore used instead of wrapper.withRedux(App) due to deprecation warning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,19 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Noto_Sans_JP } from '@next/font/google'
 import { wrapper } from '../redux/store/store'
+import { Provider } from 'react-redux'
 
 const notoSans = Noto_Sans_JP({ weight: ['300', '400', '700', '900'], subsets: ['latin'] })
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(App)
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </Provider>
   )
 }
 
-export default wrapper.withRedux(App)
+export default App
