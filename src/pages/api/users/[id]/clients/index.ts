12387f24e6ca643337d4057a1d6e069fca75a1ac
Matthew Powell <matt.powell2784@gmail.com>
2023-02-27 19:37:16 +0000

    users/id/clients route refactor

--- src/pages/api/users/[id]/clients/index.ts
+++ src/pages/api/users/[id]/clients/index.ts
@@ -4,32 +4,30 @@ import { authOptions } from '../../../auth/[...nextauth]'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getServerSession(req, res, authOptions)
   const { method } = req
-  const user_id = req.query.id
 
   try {
-    if (session && method === 'GET') {
+    if (method === 'GET') {
+      const session = await getServerSession(req, res, authOptions)
       const email = session?.user?.email
+      const user_id = req.query.id?.toString()
 
       if (!email) {
         res.status(403).json({ success: false, status: 403, errors: [{ msg: 'User not found' }] })
       }
 
-      if (email && !Array.isArray(user_id)) {
-        const clients = await prisma.user.findUnique({
-          where: { id: user_id },
-          include: { clients: true },
-        })
+      const user = await prisma.user.findUnique({
+        where: { id: user_id },
+        select: { clients: true },
+      })
 
-        const clientsList = clients?.clients
+      const clientsList = user?.clients
 
-        res.status(201).json({
-          success: true,
-          status: 201,
-          data: clientsList,
-        })
-      }
+      res.status(201).json({
+        success: true,
+        status: 201,
+        data: clientsList,
+      })
     }
   } catch (err: any) {
     console.error(err.message)
