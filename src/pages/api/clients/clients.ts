d4cfc154bd8ece2ee6d712a86fbccc62d3c79d2b
Matthew Powell <matt.powell2784@gmail.com>
2023-02-27 19:51:27 +0000

    /clients route refactor

--- src/pages/api/clients/clients.ts
+++ src/pages/api/clients/clients.ts
@@ -4,23 +4,25 @@ import { authOptions } from '../auth/[...nextauth]'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getServerSession(req, res, authOptions)
   const { method } = req
-  const { user_id, name, company_name, add1, add2, post_code } = req.body
-  const email = session?.user?.email
 
   try {
-    if (email && method === 'POST') {
-      const newClient = await prisma.client.create({
-        data: { user_id: user_id, name, company_name, add1, add2, post_code },
-      })
+    if (method === 'POST') {
+      const session = await getServerSession(req, res, authOptions)
+      const { user_id, name, company_name, add1, add2, post_code } = req.body
 
-      res.status(201).json({
-        success: true,
-        status: 201,
-        msg: 'New Client added to database',
-        data: newClient,
-      })
+      if (session) {
+        const newClient = await prisma.client.create({
+          data: { user_id: user_id, name, company_name, add1, add2, post_code },
+        })
+
+        res.status(201).json({
+          success: true,
+          status: 201,
+          msg: 'New Client added to database',
+          data: newClient,
+        })
+      }
     }
   } catch (err: any) {
     console.error(err.message)
