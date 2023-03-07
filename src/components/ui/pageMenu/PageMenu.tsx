0fbdb3ac4b2df477e088aacf79e12d12930474ee
Matthew Powell <matt.powell2784@gmail.com>
2023-03-07 19:32:35 +0000

    page menu stash

--- src/components/ui/pageMenu/PageMenu.tsx
+++ src/components/ui/pageMenu/PageMenu.tsx
@@ -8,7 +8,7 @@ interface MenuItem {
 
 interface PageMenuProps {
   title: string
-  menuItem?: MenuItem
+  menuItems?: MenuItem[]
 }
 
 export const PageMenu: React.FC<PageMenuProps> = ({ title, menuItems }) => {
@@ -45,24 +45,6 @@ export const PageMenu: React.FC<PageMenuProps> = ({ title, menuItems }) => {
     </Fragment>
   )
 }
-
-const FlexboxStyled = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  width: 100vw;
-  min-width: 310px;
-  padding: 0 2rem;
-  margin: 0.5rem auto;
-  @media only screen and (max-width: 441px) {
-    flex-direction: column;
-    margin: 0.5rem auto;
-    padding: unset;
-  }
-`
 
 const StyledH2 = styled.h2`
   font-size: 2rem;
